c504bf22980554dcf775f470f249bbb4fa41e916
Ythnam <phildesalondepce@hotmail.fr>
2024-12-04 16:45:51 +0100

    [FABCAC-2] - Setup Prettier

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,7 +1,7 @@
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  let fixture : AppComponent;
+  let fixture: AppComponent;
 
   beforeEach(() => {
     fixture = new AppComponent();
