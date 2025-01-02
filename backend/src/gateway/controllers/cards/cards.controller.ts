e7d28aa0bec3923bf6496fef24436618e5903bd4
Ythnam <phildesalondepce@hotmail.fr>
2025-01-02 16:45:45 +0100

    [FABCAC-8] - Implement getAllCardsBySet

--- backend/src/gateway/controllers/cards/cards.controller.ts
+++ backend/src/gateway/controllers/cards/cards.controller.ts
@@ -1,15 +1,26 @@
-import { GetAllCardsUseCase } from '@/domain/use-cases/cards/get-all-cards.usecase';
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ApiOkResponse } from '@nestjs/swagger';
+
 import { CardDto } from './dto/card.dto';
+import { GetAllCardsUseCase } from '@/domain/use-cases/cards/get-all-cards.usecase';
+import { GetAllCardsFilteredBySetUseCase } from '@/domain/use-cases/cards/get-all-cards-filtered-by-set.usecase';
 
 @Controller('cards')
 export class CardsController {
-  constructor(private readonly getAllCardsUseCase: GetAllCardsUseCase) {}
+  constructor(
+    private readonly getAllCardsUseCase: GetAllCardsUseCase,
+    private readonly getAllCardsFilteredBySetUseCase: GetAllCardsFilteredBySetUseCase,
+  ) {}
 
   @Get()
   @ApiOkResponse({ type: Array<CardDto> })
   findAll() {
     return this.getAllCardsUseCase.execute();
+  }
+
+  @Get(':set')
+  @ApiOkResponse({ type: Array<CardDto> })
+  findAllCardsFilteredBySet(@Query('set') set: string) {
+    return this.getAllCardsFilteredBySetUseCase.execute(set);
   }
 }
--- backend/src/infrastructure/dataset/repositories/cards.repository.ts
+++ backend/src/infrastructure/dataset/repositories/cards.repository.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
+
 import { cards } from '@flesh-and-blood/cards';
-import { Card as FABCard } from '@flesh-and-blood/types';
+import { Card as FABCard, Release } from '@flesh-and-blood/types';
+
 import { ICard } from '@/domain/entities/cards/card.interface';
 import { ICardsRepository } from '@/domain/repositories/cards-repository.interface';
 import { CardEntityMapper } from '../mapper/card-entity-mapper';
@@ -12,11 +14,32 @@ export class CardsRepository implements ICardsRepository {
   findAll(): Promise<ICard[]> {
     // TODO: Remove the slice and optimise the call
     const test = cards.slice(0, 100);
-    const cardsMapped = test.map((card: FABCard) => {
+    const cardEntities = this.convertToCardEntity(test);
+    const result = this.convertToPromise<ICard[]>(cardEntities);
+    return result;
+  }
+
+  findAllCardsFileredBySet(set: string): Promise<Array<ICard>> {
+    const filteredCards = this.filterCardsBySet(set);
+    const cardEntities = this.convertToCardEntity(filteredCards);
+    const result = this.convertToPromise<ICard[]>(cardEntities);
+    return result;
+  }
+
+  private filterCardsBySet(set: string): FABCard[] {
+    return cards.filter((card: FABCard) => card.sets.includes(set as Release));
+  }
+
+  private convertToCardEntity(card: FABCard[]): Array<ICard> {
+    const cardsMapped = card.map((card: FABCard) => {
       return CardEntityMapper.toCardEntity(card);
     });
-    const result = new Promise<ICard[]>((resolve) => {
-      resolve(cardsMapped);
+    return cardsMapped;
+  }
+
+  private convertToPromise<T>(element: T): Promise<T> {
+    const result = new Promise<T>((resolve) => {
+      resolve(element);
     });
     return result;
   }
