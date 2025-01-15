3b4353bf7bf09606458c2ed5af609c44b988ceea
Ythnam <phildesalondepce@hotmail.fr>
2025-01-15 16:02:03 +0100

    [FABCAC-15] - Implement frontend architecture

---
+++ frontend/src/app/layouts/header/main-header/main-header.component.spec.ts
@@ -0,0 +1,13 @@
+import { MainHeaderComponent } from './main-header.component';
+
+describe('MainHeaderComponent', () => {
+  let component: MainHeaderComponent;
+
+  beforeEach(() => {
+    component = new MainHeaderComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
