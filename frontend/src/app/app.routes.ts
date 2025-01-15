55030e96a2390d4ad0f2b07fdbc4fa45cae1e763
Ythnam <phildesalondepce@hotmail.fr>
2025-01-15 20:50:03 +0100

    Adding routes and main modules

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeMainComponent } from './modules/home/components/home-main/home-main.component';
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeMainComponent,
+        children: [
+            {
+                path: 'home',
+                loadChildren: () => import('../app/modules/home/home.module').then((m) => m.HomeModule),
+            },
+            {
+                path: 'search',
+                loadChildren: () => import('../app/modules/search-cards/search-cards.module').then((m) => m.SearchCardsModule),
+            },
+        ]
+    }
+];
