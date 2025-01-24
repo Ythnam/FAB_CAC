c6249fcb34e3e557ffaaf24b80947f47039799ba
Ythnam <phildesalondepce@hotmail.fr>
2025-01-24 19:52:39 +0100

    Update theme & generate card list

--- frontend/src/app/modules/search-cards/search-cards.module.ts
+++ frontend/src/app/modules/search-cards/search-cards.module.ts
@@ -7,9 +7,10 @@ import { SearchCardsRoutingModule } from './search-cards-routing.module';
 import { SearchCardsMainComponent } from './components/search-cards-main/search-cards-main.component';
 import { SearchCardsHeaderComponent } from './components/search-cards-header/search-cards-header.component';
 import { SearchCardsFormComponent } from './components/search-cards-form/search-cards-form.component';
+import { SearchCardsListComponent } from './components/search-cards-list/search-cards-list.component';
 
 @NgModule({
-  declarations: [SearchCardsMainComponent, SearchCardsHeaderComponent, SearchCardsFormComponent],
+  declarations: [SearchCardsMainComponent, SearchCardsHeaderComponent, SearchCardsFormComponent, SearchCardsListComponent],
   imports: [CommonModule, RouterModule, SearchCardsRoutingModule, SharedModule],
   exports: [SearchCardsMainComponent],
 })
