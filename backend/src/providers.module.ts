2e9534dbf4974f32167bfe60113c140b42b64fcb
Philippe Jacob <phildesalondepce@hotmail.fr>
2025-02-05 10:32:05 +0100

    [FABCAC-23] - Add backend env configuration (#25)

--- backend/src/providers.module.ts
+++ backend/src/providers.module.ts
@@ -1,11 +1,18 @@
 import { Global, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { CARDS_REPOSITORY } from './constants';
 import { CardsRepository } from './infrastructure/dataset/repositories/cards.repository';
 import { DatasetModule } from './infrastructure/dataset/dataset.module';
 
 @Global()
 @Module({
-  imports: [DatasetModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatasetModule,
+  ],
   providers: [
     {
       provide: CARDS_REPOSITORY,
