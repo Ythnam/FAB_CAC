a208ea615085c8c365a2c94c226c4a3eac90bd5f
Ythnam <phildesalondepce@hotmail.fr>
2025-02-05 10:29:21 +0100

    [FABCAC-23] - Add backend env configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -9,11 +9,11 @@ import { ControllersModule } from './gateway/controllers/controllers.module';
     ControllersModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'example',
-      database: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       entities: ['dist/**/*.entity{.ts,.js}'],
       synchronize: true, // TODO: To be removed in PROD
     }),
