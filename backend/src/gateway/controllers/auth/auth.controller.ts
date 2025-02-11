c8c0ab3d7eb9cda86076651c9a9b8b68f78f5a0b
Ythnam <phildesalondepce@hotmail.fr>
2025-02-11 18:42:29 +0100

    Log on Swagger

--- backend/src/gateway/controllers/auth/auth.controller.ts
+++ backend/src/gateway/controllers/auth/auth.controller.ts
@@ -1,14 +1,16 @@
 import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiOAuth2, ApiTags } from '@nestjs/swagger';
 
 import { AuthenticateUserUseCase } from '@/domain/use-cases/auth/authenticate-user.use-case';
 import { RegisterLocalUserUseCase } from '@/domain/use-cases/auth/register-local-user.use-case';
 import { LoginLocalUserUseCase } from '@/domain/use-cases/auth/login-local-user.use-case';
 import { RegisterDto } from './dtos/register.dto';
 import { LoginDto } from './dtos/login.dto';
-import { AuthGuard } from '@nestjs/passport';
 import { AuthProvider } from '@/domain/enums/auth-provider.enum';
 
 @Controller('auth')
+@ApiTags('auth')
 export class AuthController {
   constructor(
     private readonly authenticateSocialUserUseCase: AuthenticateUserUseCase,
@@ -46,6 +48,7 @@ export class AuthController {
     return this.authenticateSocialUserUseCase.execute(AuthProvider.FACEBOOK, req.user);
   }
 
+  @ApiOAuth2(['read'])
   @Get('me')
   @UseGuards(AuthGuard('jwt'))
   getProfile(@Req() req) {
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,20 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const config = new DocumentBuilder().setTitle('FaB CaC').setDescription('The FaB Cac API description').setVersion('1.0').build();
+  const config = new DocumentBuilder()
+    .setTitle('FaB CaC')
+    .setDescription('The FaB Cac API description')
+    .setVersion('1.0')
+    .addOAuth2({
+      type: 'oauth2',
+      flows: {
+        password: {
+          tokenUrl: 'http://localhost:3000/auth/login',
+          scopes: {},
+        },
+      },
+    })
+    .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
